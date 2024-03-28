5371eba746dcb88222e13cb20e1271a67daa8e13
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-03-29 00:09:30 +0100

    Basic CRUD with NestJs

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class CarsService {
@@ -20,11 +20,14 @@ export class CarsService {
     },
   ];
 
-  findAll(){
+  findAll() {
     return this.cars;
   }
 
-  findOneById(id: number){
-    return this.cars.find((car) => car.id === id);
+  findOneById(id: number) {
+    const car = this.cars.find((car) => car.id === id);
+    if (!car) throw new NotFoundException(`Car with ID '${id}' not found`);
+
+    return car;
   }
 }
